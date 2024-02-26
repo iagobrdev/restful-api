6589d16413a7e02f7418876e91d9d7dd7793804c
Iago Ribeiro <iagobertolettiribeiro@gmail.com>
2024-02-26 14:10:31 -0300

    docker compose

--- src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
+++ src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('leandro', 'Leandro Costa', '19bbf735b27066f2f145e602624e1b24a3fbc54cd5dfd3143fc5feea6bdee9e139ca7332d4806b9f', b'1', b'1', b'1', b'1'),
-	('flavio', 'Flavio Costa', '75ec349c1b0ef4ee7b249d0b83ae4861853f3aa77bce8c4b15f28cd43c6424ab4f29df431831bb0d', b'1', b'1', b'1', b'1');
+	('iago', 'Iago Ribeiro', 'bb92b3d3d078fbadf2892b57cfcf1525df463694bcafddff8aa6865d4bf9e22c12e2cb8847b7609a', b'1', b'1', b'1', b'1'),
+	('visitante', 'Visitante', 'bb92b3d3d078fbadf2892b57cfcf1525df463694bcafddff8aa6865d4bf9e22c12e2cb8847b7609a', b'1', b'1', b'1', b'1');
