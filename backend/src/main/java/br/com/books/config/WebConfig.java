83606738a7c0bd0cbe5f983ec7beb2c5db7aabb0
Iago Ribeiro <iagobertolettiribeiro@gmail.com>
2024-02-29 16:30:39 -0300

    cors resolved

--- backend/src/main/java/br/com/books/config/WebConfig.java
+++ backend/src/main/java/br/com/books/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		        .allowedOrigins("*")
+		        .allowedOrigins("https://iagobrdev.com.br")
 		        .allowedMethods("*")
 		        .allowedHeaders("*")
 		        .allowCredentials(true);
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,14 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: 'https://books.iagobrdev.com.br',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+instance.interceptors.request.use(config => {
+    config.headers['Origin'] = 'https://iagobrdev.com.br';
+    return config;
 });
 
 export default api;
